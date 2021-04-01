0af717d9da94268e56be4e3361b6313d976a8447
Vladislav Plotnikov <vladik1209@gmail.com>
2021-04-01 23:55:35 +0300

    приемлемо распознает

--- realtime_human_action_recognition.py
+++ realtime_human_action_recognition.py
@@ -15,19 +15,17 @@ from poseEstimation.modules.keypoints import extract_keypoints, group_keypoints
 from poseEstimation.models.with_mobilenet import PoseEstimationWithMobileNet
 
 LABELS = ["ComeHere/Watch/Me",
-          "Danger/You",
+          "Danger/You/Help",
           "DontKnow",
           "OKsurface",
-          "Think",
+          "Think/PressureBalancePb/ReserveOpened/Stop",
           "CannotOpenReserve",
           "Cold",
-          "Help",
-          "Meet",
-          "PressureBalancePb/ReserveOpened/Stop"]
+          "Meet"]
 
 keypoints_to_delete = [18, 19, 20, 21, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
-N_CLASSES = 10
+N_CLASSES = 8
 INPUT_DIM = 20
 SEQUENCE_LENGTH = 16
 
